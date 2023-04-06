640dded646aa04cf45a8c9c0e08154249c257fca
harsh1532 <patelharsh1532@gmail.com>
2023-04-06 17:47:20 -0400

    second commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "f39b63c679msh8aafe787799b0ecp1fb019jsn00d66986bfa8",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8c9f17b0793cdf02a376895a736d73be"; // enter your key from openweather API
